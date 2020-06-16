998c13430ca793162dee4e0cc7408cbda7aaee72
timbotimber <hitimstephens@gmail.com>
2020-06-16 15:39:16 +0200

    tiiiny change

--- reward.js
+++ reward.js
@@ -1,13 +1,14 @@
 class Reward {
-    constructor(x, y) {
-        this.x= 400;               //setting starting point of the apple
-        this.y= 400;
-    }
-    setRandomBreze() {
-        this.x=Math.floor(Math.random() * 30) * side; 
-        this.y=Math.floor(Math.random() * 30)* side; 
-    }
-    drawReward() {
-        image(rewardImg, this.y, this.x, side, side)
-    }
+  constructor(x, y) {
+    this.x = 400; //setting starting point of the apple
+    this.y = 400;
+  }
+  setRandomBreze() {
+    this.x = Math.floor(Math.random() * 30) * side;
+    this.y = Math.floor(Math.random() * 30) * side;
+  }
+  drawReward() {
+    image(rewardImg, this.x, this.y, side, side);
+    //console.log("y value:", this.y);
+  }
 }
